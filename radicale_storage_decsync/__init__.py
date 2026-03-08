d84e5b17c898d9f87bf88c63407f8a9811244a32
Xander <dxvxlopxr@outlook.com>
2026-03-08 17:24:30 +0300

    v2.2.0: Radicale 3.6.1 Support (Small Сompatibility Fixes)

--- radicale_storage_decsync/__init__.py
+++ radicale_storage_decsync/__init__.py
@@ -10,13 +10,15 @@ from radicale import item as radicale_item
 import radicale.storage.multifilesystem as storage
 from libdecsync import Decsync
 
+
 def _get_attributes_from_path(path):
     attributes = pathutils.strip_path(path).split("/")
     if not attributes[0]:
         attributes.pop()
     return attributes
 
-class CollectionHrefMappingsMixin:
+
+class CollectionHrefMappingsMixin(storage.Collection):
     def load_hrefs(self, sync_type):
         if sync_type == "contacts":
             self._suffix = ".vcf"
@@ -44,9 +46,10 @@ class CollectionHrefMappingsMixin:
             json.dump(self._hrefs, f)
 
     def get_uid(self, href):
-        return self._uids.get(href, href[:-len(self._suffix)])
+        return self._uids.get(href, href[: -len(self._suffix)])
 
-class Collection(storage.Collection, CollectionHrefMappingsMixin):
+
+class Collection(CollectionHrefMappingsMixin, storage.Collection):
     def __init__(self, storage_, path, filesystem_path=None):
         super().__init__(storage_, path, filesystem_path=filesystem_path)
         attributes = _get_attributes_from_path(path)
@@ -55,7 +58,7 @@ class Collection(storage.Collection, CollectionHrefMappingsMixin):
             sync_type = attributes[1].split("-")[0]
             if sync_type not in ["contacts", "calendars", "tasks", "memos"]:
                 raise RuntimeError("Unknown sync type " + sync_type)
-            collection = attributes[1][len(sync_type)+1:]
+            collection = attributes[1][len(sync_type) + 1 :]
             own_app_id = Decsync.get_app_id("Radicale")
             self.decsync = Decsync(decsync_dir, sync_type, collection, own_app_id)
 
@@ -66,9 +69,12 @@ class Collection(storage.Collection, CollectionHrefMappingsMixin):
                     if value:
                         extra.delete(update_decsync=False)
                 elif key == "color":
-                    extra._set_meta_key("ICAL:calendar-color", value, update_decsync=False)
+                    extra._set_meta_key(
+                        "ICAL:calendar-color", value, update_decsync=False
+                    )
                 else:
                     raise ValueError("Unknown info key " + key)
+
             self.decsync.add_listener(["info"], info_listener)
 
             def resources_listener(path, datetime, key, value, extra):
@@ -86,24 +92,36 @@ class Collection(storage.Collection, CollectionHrefMappingsMixin):
                     else:
                         tag = "VCALENDAR"
                     radicale_item.check_and_sanitize_items([vobject_item], tag=tag)
-                    item = radicale_item.Item(collection=extra, vobject_item=vobject_item, uid=uid)
+                    item = radicale_item.Item(
+                        collection=extra, vobject_item=vobject_item, uid=uid
+                    )
                     item.prepare()
                     extra.upload(href, item, update_decsync=False)
+
             self.decsync.add_listener(["resources"], resources_listener)
 
             self.load_hrefs(sync_type)
 
-    def upload(self, href, orig_item, update_decsync=True):
-        item = super().upload(href, orig_item)
+    def upload(self, href, item, update_decsync=True):
+        item = super().upload(href, item)
         if update_decsync:
             tag = self.get_meta("tag")
             if tag == "VCALENDAR":
-                supported_components = (self.get_meta("C:supported-calendar-component-set") or "VEVENT,VTODO,VJOURNAL").split(",")
-                component_name = item.component_name
+                supported_components = (
+                    self.get_meta("C:supported-calendar-component-set")
+                    or "VEVENT,VTODO,VJOURNAL"
+                ).split(",")
+                component_name = item[0].component_name
                 if len(supported_components) > 1 and component_name != "VEVENT":
-                    raise RuntimeError("Component " + component_name + " is not supported by old DecSync collections. Create a new collection in Radicale for support.")
-            self.set_href(item.uid, href)
-            self.decsync.set_entry(["resources", item.uid], None, item.serialize())
+                    raise RuntimeError(
+                        "Component "
+                        + component_name
+                        + " is not supported by old DecSync collections. Create a new collection in Radicale for support."
+                    )
+            self.set_href(item[0].uid, href)
+            self.decsync.set_entry(
+                ["resources", item[0].uid], None, item[0].serialize()
+            )
         return item
 
     def delete(self, href=None, update_decsync=True):
@@ -146,18 +164,26 @@ class Collection(storage.Collection, CollectionHrefMappingsMixin):
             self.decsync.execute_all_new_entries(self)
         return super().sync(old_token)
 
+
 class Storage(storage.Storage):
     _collection_class = Collection
 
     def __init__(self, configuration):
         super().__init__(configuration)
         try:
-            self.decsync_dir = os.path.expanduser(configuration.get("storage", "decsync_dir"))
+            self.decsync_dir = os.path.expanduser(
+                configuration.get("storage", "decsync_dir")
+            )
         except KeyError:
             self.decsync_dir = ""
 
-    def discover(self, path, depth="0", child_context_manager=(
-            lambda path, href=None: contextlib.ExitStack())):
+    def discover(
+        self,
+        path,
+        depth="0",
+        child_context_manager=(lambda path, href=None: contextlib.ExitStack()),
+        user_groups=set(),
+    ):
         collections = list(super().discover(path, depth, child_context_manager))
         for collection in collections:
             yield collection
@@ -177,7 +203,12 @@ class Storage(storage.Storage):
                     child_path = "/%s/%s-%s/" % (username, sync_type, collection)
                     if pathutils.strip_path(child_path) in known_paths:
                         continue
-                    if Decsync.get_static_info(self.decsync_dir, sync_type, collection, "deleted") == True:
+                    if (
+                        Decsync.get_static_info(
+                            self.decsync_dir, sync_type, collection, "deleted"
+                        )
+                        == True
+                    ):
                         continue
 
                     props = {}
@@ -196,7 +227,9 @@ class Storage(storage.Storage):
                     child = super().create_collection(child_path, props=props)
                     child.decsync.init_stored_entries()
                     child.decsync.execute_stored_entries_for_path_exact(["info"], child)
-                    child.decsync.execute_stored_entries_for_path_prefix(["resources"], child)
+                    child.decsync.execute_stored_entries_for_path_prefix(
+                        ["resources"], child
+                    )
                     yield child
         elif len(attributes) == 2:
             return
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="radicale_storage_decsync",
-    version="2.1.0",
+    version="2.2.0",
     author="Aldo Gunsing",
     author_email="dev@aldogunsing.nl",
     url="https://github.com/39aldo39/Radicale-DecSync",
@@ -14,15 +14,12 @@ setup(
     keywords=["decsync", "radicale"],
     license="GPLv3+",
     packages=["radicale_storage_decsync"],
-    install_requires=[
-        "radicale>=3",
-        "libdecsync>=2.0.0"
-    ],
+    install_requires=["radicale>=3", "libdecsync>=2.0.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: POSIX :: Linux",
         "Operating System :: Microsoft :: Windows",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Intended Audience :: End Users/Desktop"
-    ]
+        "Intended Audience :: End Users/Desktop",
+    ],
 )
